f594e191068ef187d7f09d60bfa9fbdf8f88cf0d
Mohammad Khaseeb <mohammadkhaseeb2@outlook.com>
2025-05-11 16:52:51 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let rightAnswers = 0;
 let countdownInterval;
 
 function getQuestion() {
-  fetch("../questions.json")
+  fetch("https://m7amd1.github.io/Quiz-App/html_questions.json")
     .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
